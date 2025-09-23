4ef6350e3a86b540c88ef431ada45dac652c14f9
Naeem Malik <malik2015naeem@gmail.com>
2025-09-23 10:42:54 +0000

    pacman mirrors fix

--- fix-mirrors.sh
+++ fix-mirrors.sh
@@ -1,11 +1,105 @@
 #!/usr/bin/env bash
 # fix-mirrors.sh
-# Safe helper to ensure Arch Linux ARM mirrors are present in /etc/pacman.d/mirrorlist
+# Safe helper to ensure proper pacman configuration and mirrors are set up
 # Supports: --replace (force overwrite), --prefer (place Omarchy servers at top),
 # --backup (create timestamped backup), --dry-run (show actions, don't write)
 # Usage: sudo ./fix-mirrors.sh [--replace] [--prefer] [--backup] [--dry-run]
 
 set -euo pipefail
+
+# Setup pacman.conf first
+cat > /tmp/pacman.conf << 'EOL'
+# /etc/pacman.conf
+#
+# See the pacman.conf(5) manpage for option and repository directives
+
+#
+# GENERAL OPTIONS
+#
+[options]
+# The following paths are commented out with their default values listed.
+# If you wish to use different paths, uncomment and update the paths.
+#RootDir     = /
+#DBPath      = /var/lib/pacman/
+#CacheDir    = /var/cache/pacman/pkg/
+#LogFile     = /var/log/pacman.log
+#GPGDir      = /etc/pacman.d/gnupg/
+#HookDir     = /etc/pacman.d/hooks/
+HoldPkg     = pacman glibc
+#XferCommand = /usr/bin/curl -L -C - -f -o %o %u
+#XferCommand = /usr/bin/wget --passive-ftp -c -O %o %u
+#CleanMethod = KeepInstalled
+Architecture = aarch64
+
+# Pacman won't upgrade packages listed in IgnorePkg and members of IgnoreGroup
+#IgnorePkg   =
+#IgnoreGroup =
+
+#NoUpgrade   =
+#NoExtract   =
+
+# Misc options
+#UseSyslog
+#Color
+#NoProgressBar
+CheckSpace
+#VerbosePkgLists
+ParallelDownloads = 5
+
+# By default, pacman accepts packages signed by keys that its local keyring
+# trusts (see pacman-key and its man page), as well as unsigned packages.
+SigLevel    = Required DatabaseOptional
+LocalFileSigLevel = Optional
+#RemoteFileSigLevel = Required
+
+#
+# REPOSITORIES
+#   - can be defined here or included from another file
+#   - pacman will search repositories in the order defined here
+#   - local/custom mirrors can be added here or in separate files
+#   - repositories listed first will take precedence when packages
+#     have identical names, regardless of version number
+#   - URLs will have $repo replaced by the name of the current repo
+#   - URLs will have $arch replaced by the name of the architecture
+#
+# Repository entries are of the format:
+#       [repo-name]
+#       Server = ServerName
+#       Include = IncludePath
+#
+# The header [repo-name] is crucial - it must be present and
+# uncommented to enable the repo.
+#
+
+# The testing repositories are disabled by default. To enable, uncomment the
+# repo name header and Include lines. You can add preferred servers immediately
+# after the header, and they will be used before the default mirrors.
+
+[asahi-alarm]
+Include = /etc/pacman.d/mirrorlist.asahi-alarm
+
+[core]
+Include = /etc/pacman.d/mirrorlist
+
+[extra]
+Include = /etc/pacman.d/mirrorlist
+
+[community]
+Include = /etc/pacman.d/mirrorlist
+
+[alarm]
+Include = /etc/pacman.d/mirrorlist
+
+[aur]
+Include = /etc/pacman.d/mirrorlist
+EOL
+
+# Install the pacman.conf if it doesn't match
+if ! cmp -s /tmp/pacman.conf /etc/pacman.conf; then
+    echo "Installing new pacman.conf..."
+    sudo cp /tmp/pacman.conf /etc/pacman.conf
+fi
+rm -f /tmp/pacman.conf
 
 SRC="$HOME/.local/share/omarchy/default/pacman/mirrorlist"
 DEST="/etc/pacman.d/mirrorlist"
