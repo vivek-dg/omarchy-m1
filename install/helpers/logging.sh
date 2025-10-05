bc18a0f1a3f40336ddef6b11bdbe7a66738a191b
malik-na <malik2015naeem@gmail.com>
2025-10-06 02:17:10 +0530

    Add fallback logging for total installation time when Archinstall data is unavailable

--- install/helpers/logging.sh
+++ install/helpers/logging.sh
@@ -104,6 +104,9 @@ stop_install_log() {
         TOTAL_MINS=$((TOTAL_DURATION / 60))
         TOTAL_SECS=$((TOTAL_DURATION % 60))
         echo "Total:       ${TOTAL_MINS}m ${TOTAL_SECS}s" >>"$OMARCHY_INSTALL_LOG_FILE"
+      else
+        # If no Archinstall data, use Omarchy time as total
+        echo "Total:       ${OMARCHY_MINS}m ${OMARCHY_SECS}s" >>"$OMARCHY_INSTALL_LOG_FILE"
       fi
     fi
     echo "=================================" >>"$OMARCHY_INSTALL_LOG_FILE"
