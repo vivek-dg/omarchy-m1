e61a53a8845cfabfc6e6c5bf4ac0a6e51fc389f4
Vivekanand D <vivekanand.dorairaj@gmail.com>
2025-10-26 11:41:24 -0700

    make sure sudo is intalled. also explicitly print the branch used

--- boot.sh
+++ boot.sh
@@ -17,10 +17,10 @@ ansi_art='                 ▄▄▄
 clear
 echo -e "\n$ansi_art\n"
 
+sudo pacman -Syu --noconfirm --needed sudo git
+
 # Validate sudo access and refresh timestamp to minimize password prompts
 sudo -v
-
-sudo pacman -Syu --noconfirm --needed git
 
 # Use custom repo if specified, otherwise default to vivek-dg/omarchy-m1
 OMARCHY_REPO="${OMARCHY_REPO:-vivek-dg/omarchy-m1}"
@@ -31,8 +31,8 @@ git clone "https://github.com/${OMARCHY_REPO}.git" ~/.local/share/omarchy >/dev/
 
 # Use custom branch if instructed, otherwise default to main
 OMARCHY_REF="${OMARCHY_REF:-main}"
+echo -e "\e[32mUsing branch: $OMARCHY_REF\e[0m"
 if [[ $OMARCHY_REF != "main" ]]; then
-  echo -e "\e[32mUsing branch: $OMARCHY_REF\e[0m"
   cd ~/.local/share/omarchy
   git fetch origin "${OMARCHY_REF}" && git checkout "${OMARCHY_REF}"
   cd -
