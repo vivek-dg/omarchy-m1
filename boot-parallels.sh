84be7c5c51af19a24193e2582ac08ebd5eff3784
Vivekanand D <vivekanand.dorairaj@gmail.com>
2025-10-29 22:25:10 -0700

    update the correct urls that work for curl commands

--- boot-parallels.sh
+++ boot-parallels.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Upgrade the system, create a new user and drop into that user
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/vivek-dg/omarchy-m1/raw/refs/heads/main/prereq.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/vivek-dg/omarchy-m1/main/prereq.sh)"
 
 # Run the Omarchy script
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/vivek-dg/omarchy-m1/raw/refs/heads/main/boot.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/vivek-dg/omarchy-m1/main/boot.sh)"
